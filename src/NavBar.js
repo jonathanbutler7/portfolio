e8afcab79b5a2e828c23e2f2a8d09be904401f5e
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-01 17:58:22 -0800

    added nav and select bars

---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './NavBar.module.scss'
+import {AiOutlineCoffee} from 'react-icons/ai'
+
+function NavBar() {
+    return (
+        <nav className={styles.nav}>
+            <AiOutlineCoffee />
+            <h1>Jonathan</h1>
+        </nav>
+    )
+}
+
+export default NavBar
---
+++ src/SelectBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './SelectBar.module.scss';
+
+function SelectBar() {
+  return (
+    <div className={styles.selectBar}>
+      <section className={styles.left}>
+        <a href=''>About</a>
+        <a href=''>Projects</a>
+        <a href=''>Contact</a>
+      </section>
+      <section className={styles.right}>click me</section>
+    </div>
+  );
+}
+
+export default SelectBar;

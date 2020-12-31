e2350ac67c5badce250e8df32ceddd2a0dffde0b
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-30 20:36:55 -0800

    fixed link to dashboard

--- src/store/links/code.js
+++ src/store/links/code.js
@@ -1,7 +1,7 @@
 const notefulApp = 'https://noteful-deployment.herokuapp.com/';
 const notefulCode = 'https://github.com/jonathanbutler7/noteful';
-const dashboardApp = 'https://jonathanbutler7.github.io/dashboard-app-final/';
-const dashboardCode = 'https://github.com/jonathanbutler7/dashboard-app-final';
+const dashboardApp = 'https://serene-savannah-58732.herokuapp.com/';
+const dashboardCode = 'https://github.com/jonathanbutler7/dashboard';
 const spaceXApp = 'https://jonathanbutler7.github.io/space-x-api/';
 const spaceXCode = 'https://github.com/jonathanbutler7/space-x-api';
 

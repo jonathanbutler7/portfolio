1e6997cd2b270613ac656bc1c6f79f73a81a5a92
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-30 20:35:24 -0800

    changed noteful image

--- src/store/links/images.js
+++ src/store/links/images.js
@@ -1,6 +1,6 @@
 const flickrUrl = 'https://live.staticflickr.com/65535';
 const dashboard = `${flickrUrl}/50781288956_abaf497248_b.jpg`;
 const space_x = `${flickrUrl}/50780524053_2828e70c21_b.jpg`;
-const noteful = `${flickrUrl}/50781396982_e00a846aa3_b.jpg`;
+const noteful = `${flickrUrl}/50781958197_f5b1180dee_b.jpg`;
 
 export { dashboard, noteful, space_x };

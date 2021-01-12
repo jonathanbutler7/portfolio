46b71e3c8e7ec7188354737ff45f1f7330d54e27
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-11 16:16:37 -0800

    updates resume link in store/assets/contact

--- src/store/assets/code.js
+++ src/store/assets/code.js
@@ -1,9 +1,11 @@
+const github = 'https://github.com/jonathanbutler7';
+
 const notefulApp = 'https://noteful-deployment.herokuapp.com/';
-const notefulCode = 'https://github.com/jonathanbutler7/noteful';
 const dashboardApp = 'https://serene-savannah-58732.herokuapp.com/';
-const dashboardCode = 'https://github.com/jonathanbutler7/dashboard';
 const spaceXApp = 'https://jonathanbutler7.github.io/space-x-api/';
-const spaceXCode = 'https://github.com/jonathanbutler7/space-x-api';
+const notefulCode = `${github}/noteful`;
+const dashboardCode = `${github}/dashboard`;
+const spaceXCode = `${github}/space-x-api`;
 
 export {
   notefulApp,
--- src/store/assets/contact.js
+++ src/store/assets/contact.js
@@ -1,6 +1,6 @@
 const linkedIn = 'https://www.linkedin.com/in/jonathan-butler-9960a91a8/';
 const github = 'https://github.com/jonathanbutler7';
 const email = 'mailto:jonathanbutler7@gmail.com';
-const resume = 'https://drive.google.com/file/d/1DM13RzNAb7OVdPjgMyFjPeMjNrINV_9C/view?usp=sharing';
+const resume = 'https://drive.google.com/file/d/183rv4kvR-zWX9RYC7_a8YLuej-RRbRhT/view?usp=sharing';
 
 export { linkedIn, github, email, resume };
